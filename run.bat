76f630ce40f0a378f84dfd2ec73d05b469805a0b
Jakub Augustýn <kubik.augustyn@post.cz>
2022-12-06 18:25:47 +0100

    Fixed run.bat not working The timeout didn't work

--- run.bat
+++ run.bat
@@ -1,10 +1,13 @@
 @echo off
 taskkill /IM Mindustry.exe
 echo Killed Mindustry
-timeout 5 > nul
+PING -n 3 127.0.0.1 > nul
 echo Waited
-move "C:\Users\Radek Augustyn\Desktop\Kubik\MindustryToolkit\build\libs\*.*" "C:\Users\Radek Augustyn\AppData\Roaming\Mindustry\mods"
+move "C:\Users\Radek Augustyn\Desktop\Kubik\MindustryToolkit\build\libs\*.*" "C:\Users\Radek Augustyn\AppData\Roaming\Mindustry\mods\"
 echo Moved
+rmdir /s /q "C:\Users\Radek Augustyn\Desktop\Kubik\MindustryToolkit\build\"
+rmdir /s /q "C:\Users\Radek Augustyn\Desktop\Kubik\MindustryToolkit\.gradle\"
+echo Cleaned up
 echo Starting...
 start /B /d "C:\KubaPrograms\Mindustry\relase-140.3" Mindustry.exe
 echo Started
